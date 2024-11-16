a45cd043a02b5bcaea1c72fdf43dd2fa21dded74
NeroVCaynic <86295984+NeroVCaynic@users.noreply.github.com>
2024-11-16 16:09:32 -0500

    Connected and reading from postgres

--- modules/country-service.js
+++ modules/country-service.js
@@ -1,10 +1,10 @@
 const env = require('dotenv').config();
 const pg = require('pg'); // explicitly require the "pg" module
 const Sequelize = require('sequelize');
-const countryData = require("../data/countryData");
-const subRegionData = require("../data/subRegionData");
+// const countryData = require("../data/countryData");
+// const subRegionData = require("../data/subRegionData");
 
-let countries = [];
+// let countries = [];
 let sequelize = new Sequelize(
   env.PGDATABASE, 
   env.PGUSER, 
@@ -34,6 +34,7 @@ const SubRegion = sequelize.define(
     updatedAt: false, // disable updatedAt
   }
 );
+
 const Country = sequelize.define(
   'Country',
   {
@@ -59,94 +60,114 @@ const Country = sequelize.define(
     createdAt: false, // disable createdAt
     updatedAt: false, // disable updatedAt
   }
-)
-Country.belongsTo(SubRegion, {foreignKey: 'subRegionId'})
+);
+
+Country.belongsTo(SubRegion, {foreignKey: 'subRegionId'});
 
 function initialize() {
   return new Promise((resolve, reject) => {
-    countryData?.forEach(country => {
-      let countryWithSubRegion = { ...country, subRegionObj: subRegionData.find(sr => sr.id == country.subRegionId) }
-      countries.push(countryWithSubRegion);
+    sequelize.sync().then(()=>{
       resolve();
+    }).catch((err)=>{
+      reject(err);
     });
   });
-
 }
 
 function getAllCountries() {
   return new Promise((resolve, reject) => {
-    resolve(countries);
+    Country.findAll({ include: [SubRegion] })
+    .then((countries) => {
+      resolve(countries);
+    }).catch((err) => {
+      reject("Unable to retrieve countries");
+    });
   });
 }
 
 function getCountryById(id) {
-
   return new Promise((resolve, reject) => {
-    let foundCountry = countries.find(c => c.id == id);
-
-    if (foundCountry) {
-      resolve(foundCountry); return;
-    } else {
+    Country.findAll({
+      where: { id: id },
+      include: [SubRegion],
+    })
+    .then((countries) => {
+      if (countries.length > 0) {
+        resolve(countries[0]);
+      }
       reject("Unable to find requested country");
-    }
+    }).catch((err) => {
+      reject(err);
+    });
   });
 }
 
 function getCountriesBySubRegion(subRegion) {
-
   return new Promise((resolve, reject) => {
-    let foundCountries = countries.filter(c => c.subRegionObj.subRegion.toUpperCase().includes(subRegion.toUpperCase()));
-
-    if (foundCountries.length > 0) {
-      resolve(foundCountries)
-    } else {
-      reject(`Unable to find requested countries for the given subRegion - ${subRegion}`);
-    }
+    Country.findAll({
+      include: [SubRegion],
+      where: {
+        '$SubRegion.subRegion$': {
+          [Sequelize.Op.iLike]: `%${subRegion}%`,
+        },
+      },
+    })
+    .then((countries) => {
+      if (countries.length > 0) {
+        resolve(countries);
+      }
+      reject("Unable to find requested countries");
+    }).catch((err) => {
+      reject(err);
+    });
   });
-
 }
 
 function getCountriesByRegion(region) {
-
   return new Promise((resolve, reject) => {
-    let foundCountries = countries.filter(c => c.subRegionObj.region.toUpperCase().includes(region.toUpperCase()));
-
-    if (foundCountries.length > 0) {
-      resolve(foundCountries)
-    } else {
-      reject(`Unable to find requested countries for the given region - ${region}`);
-    }
+    Country.findAll({
+      include: [SubRegion],
+      where: {
+        '$SubRegion.region$': region,
+      },
+    })
+    .then((countries) => {
+      if (countries.length > 0) {
+        resolve(countries);
+      }
+      reject("Unable to find requested countries");
+    }).catch((err) => {
+      reject(err);
+    });
   });
-
 }
-
 
 module.exports = { initialize, getAllCountries, getCountryById, getCountriesByRegion, getCountriesBySubRegion }
 
-// Code Snippet to insert existing data from Countries / SubRegions
-sequelize
-  .sync()
-  .then( async () => {
-    try{
-      await SubRegion.bulkCreate(subRegionData);
-      await Country.bulkCreate(countryData); 
-      console.log("-----");
-      console.log("data inserted successfully");
-    }catch(err){
-      console.log("-----");
-      console.log(err.message);
+// // Code Snippet to insert existing data from Countries / SubRegions
+// sequelize
+//   .sync()
+//   .then( async () => {
+//     try{
+//       await SubRegion.bulkCreate(subRegionData);
+//       await Country.bulkCreate(countryData); 
+//       console.log("-----");
+//       console.log("data inserted successfully");
+//     }catch(err){
+//       console.log("-----");
+//       console.log(err.message);
 
-      // NOTE: If you receive the error:
+//       // NOTE: If you receive the error:
 
-      // insert or update on table "Countries" violates foreign key constraint "Countries_subRegionId_fkey"
+//       // insert or update on table "Countries" violates foreign key constraint "Countries_subRegionId_fkey"
 
-      // it is because you have a "Country" in your collection that has a "subRegionId" that does not exist in the "subRegionData".   
+//       // it is because you have a "Country" in your collection that has a "subRegionId" that does not exist in the "subRegionData".   
 
-      // To fix this, use PgAdmin to delete the newly created "SubRegions" and "Countries" tables, fix the error in your .json files and re-run this code
-    }
+//       // To fix this, use PgAdmin to delete the newly created "SubRegions" and "Countries" tables, fix the error in your .json files and re-run this code
+//     }
 
-    process.exit();
-  })
-  .catch((err) => {
-    console.log('Unable to connect to the database:', err);
-  });
+//     process.exit();
+//   })
+//   .catch((err) => {
+//     console.log('Unable to connect to the database:', err);
+//   });
